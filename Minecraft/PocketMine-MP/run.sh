548b011a33b50ce129e5c9b89007d50cc3d6d141
Batuhan Özdemir <121499687+androcyber@users.noreply.github.com>
2023-02-28 14:30:01 +0300

    Create run.sh

---
+++ Minecraft/PocketMine-MP/run.sh
@@ -0,0 +1,24 @@
+#
+#
+#   A server software based PocketMine-MP. 
+#
+#
+#                   SOCIAL MEDIA
+#   Youtube: https://www.youtube.com/androcyber
+#   Instagram: https://www.instagram.com/androcyber0
+#   Twitter: https://www.twitter.com/androcyber0
+#   Discord: https://www.discord.com/invite/EBUS4TYSY2
+#   Github: https://www.github.com/androcyber
+#
+#
+#   Made by androcyber.
+#
+cd /home/container
+
+# Startup Message
+echo "-------------------------------------------------------------------------------------------------------------"
+echo "HIVERIA | Sunucu başlatılıyor..."
+echo "-------------------------------------------------------------------------------------------------------------"
+
+# Run the Server
+eval ${STARTUP}
